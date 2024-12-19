0108064e42df76532205ba26b83f4b3a4ffa407a
Nico-HANst <nicoacole@gmail.com>
2024-12-18 21:53:52 -0300

    Second Upload

--- link_bio/views/links/links.py
+++ link_bio/views/links/links.py
@@ -4,9 +4,8 @@ from link_bio.components.link_button import link_button
 def links():
     return rx.vstack(
         link_button("GitHub", "https://github.com/Nico-HANst"),
-        link_button("YouTube", "https://www.youtube.com"),
-        link_button("Instagram", "https://www.instagram.com"),
-        link_button("Twitch", "https://www.twitch.tv"),
+        link_button("SGames_Studio", "https://scratch.mit.edu/users/SGames_Studio/"),
+        link_button("Código de Ésta Página", "https://github.com/Nico-HANst/Link-in-BIO"),
         align="center"
     )
 
